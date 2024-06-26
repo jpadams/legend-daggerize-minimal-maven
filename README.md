1982dd58fa3bca28b55a3cf6013080c791a868ee
Jeremy Adams <jeremy@dagger.io>
2024-06-26 13:05:36 -0700

    minimal working together
    Signed-off-by: Jeremy Adams <jeremy@dagger.io>

--- README.md
+++ README.md
@@ -1,26 +1,19 @@
 # Current Goal:
 ## Minimal Maven Install via `Engine`, `SDLC`, and `Studio`
 https://github.com/finos/legend/blob/master/installers/maven/README.md?plain=1#L3
-
 - [X] `Engine`
-- [ ] `SDLC`
-- [ ] `Studio`
+- [X] `SDLC`
+- [X] `Studio`
 
 <img src="legend_arch.svg" width="600" />
 
-### Setup **legend-engine**.
-Using previous source build (saved via `publish --address ...`). You probably want this one if you can download the 2.23GB image 😄
-```
-dagger call -m github.com/jpadams/legend-daggerize-minimal-maven legend-engine --use-cached-container as-service up
-```
-Purely from source (2-3 hours)
-```
-dagger call legend-engine --source https://github.com/finos/legend-engine\#master as-service up
-```
+### Setup
+1. Follow the instructions to set up a GitLab app from https://github.com/finos/legend/blob/master/installers/maven/README.md?plain=1#L17-L28
+2. Collect your Gitlab app's `APP_ID` and `APP_SECRET` and store them in environment variables
+3. Invoke `dagger call minimal --app-id env:APP_ID --app-secret env:APP_SECRET up` to bring up the services 
+4. Browse to http://localhost:9000/studio and authenticate via gitlab.com oauth/OIDC
 
-Test by going to http://localhost:6300 in a browser. The Swagger page can be accessed at http://localhost:6300/api/swagger.
-
-### ISSUES:
+### Issues:
 I was hitting the issue that several people have encountered: `java.lang.ClassNotFoundException: org.finos.legend.engine.server.Server`. While following docs, you hit a wall after successfully building the project (can take several hours). This is perhaps due to a reorg of the project/class hierarchy such that the documented classpath doesn't work. Maven (and other) docs were updated May 2023 by An Phi, but guessing An was focused on the Omnibus by commit message: https://github.com/finos/legend/tree/master/installers/maven#setup-legend-engine. An also mentions that users should use Docker images or Omnibus in most cases.
 - Might build fine in JetBrains IntelliJ (haven't tried yet).
 - Feb 2021: https://github.com/finos/legend/issues/309#issuecomment-781003612
--- dagger/src/index.ts
+++ dagger/src/index.ts
@@ -1,7 +1,13 @@
+/**
+ * Daggerized minimal Legend config
+ */
+
 import {
     dag,
     Container,
     Directory,
+	Secret,
+    Service,
     object,
     func,
 } from "@dagger.io/dagger"
@@ -9,12 +15,12 @@ import {
 @object()
 class legendDaggerizeMinimalMaven {
     /**
-     * Returns Legend Engine Container ready to
-     * run as a Service on port 6300
+     * Legend Engine build
      */
     @func()
-    base(source?: Directory, useCachedContainer: boolean=false): Container {
+    engineBase(source?: Directory, useCachedContainer: boolean=false): Container {
         const ubuntuImage = "ubuntu:jammy-20240530"
+        // cache of this portion of the build since it takes several hours on my machine
         const cachedImage = "docker.io/jeremyatdockerhub/legend-engine-poc:latest@sha256:5f2c8256faf99174998c5719c8cd35fa5c8abcbff5022efd9f3162fa56a4cae3"
 
         if (useCachedContainer === true) {
@@ -54,20 +60,129 @@ class legendDaggerizeMinimalMaven {
             .withExec(["mvn", "install", "-DskipTests"])
         }
     }
-
+    /**
+     * Legend Engine ready to run on localhost:6300
+     */
     @func()
     legendEngine(source?: Directory, useCachedContainer: boolean=false): Container {        
-        const ctr = this.base(source, useCachedContainer)
+        const ctr = this.engineBase(source, useCachedContainer)
         return ctr.withExec([
             "bash",
             "-c",
             `java -cp \
-            java -cp \
             legend-engine-config/legend-engine-server/legend-engine-server-http-server/target/legend-engine-server-http-server-*-shaded.jar \
             org.finos.legend.engine.server.Server \
             server \
             legend-engine-config/legend-engine-server/legend-engine-server-http-server/config/config.json`
         ])
-        .withExposedPort(6300)
+       .withExposedPort(6300)
     }
-}
+
+    /** 
+     * Legend SDLC build
+     */
+	@func()
+    sdlcBase(source: Directory): Container {
+        const ubuntuImage = "ubuntu:jammy-20240530"
+
+        return dag
+        .container()//{platform: "linux/amd64" as Platform})
+        .from(ubuntuImage)
+        .withExec([
+            "apt",
+            "update",
+        ])
+        .withExec([
+            "apt",
+            "install",
+            "openjdk-11-jdk",
+            "maven",
+            "curl",
+            "gettext-base",
+            "-y",
+        ])
+        // maven deps cache
+        .withMountedCache(
+            "/root/.m2/repository",
+            dag.cacheVolume("legend-engine-mvn-cache")
+        )
+        // copy in source - not mounted so we can publish all to registry
+        .withDirectory("/src", source)
+        // 2GB of heap works fine. Likely needs less.
+        .withEnvVariable("MAVEN_OPTS", "-Xmx2g")
+        .withWorkdir("/src")
+        .withExec(["mvn", "install", "-DskipTests"])
+    }
+
+    /**
+     * Legend SDLC ready to run on localhost:6100
+     */
+    @func()
+    legendSdlc(
+        source: Directory,
+        gitlabHost: string="gitlab.com",
+        sdlcServerHost: string="localhost:6100",
+        appId: Secret,
+        appSecret: Secret,
+    ): Container {
+        return this.sdlcBase(source)
+        .withEnvVariable("GITLAB_HOST", gitlabHost)
+        .withEnvVariable("SDLC_SERVER_HOST", sdlcServerHost)
+        .withSecretVariable("APP_ID", appId)
+        .withSecretVariable("APP_SECRET", appSecret)
+        .withExec(["bash", "-c", "envsubst < ./legend-sdlc-server/src/test/resources/config-sample.yaml > ./config.yaml"])
+        .withExec([
+            "bash",
+            "-c",
+            `java -cp \
+            legend-sdlc-server/target/legend-sdlc-server-*-shaded.jar \
+            org.finos.legend.sdlc.server.LegendSDLCServer \
+            server \
+            config.yaml`
+        ])
+        .withExposedPort(6100)
+        .withExposedPort(6101)      
+    }
+
+    /**
+     * Legend Studio build
+     */ 
+    @func()
+    studioBase(source: Directory): Container {
+        return dag
+        .container()
+        .from("node:20.15.0")
+        .withDirectory("/src", source)
+        .withWorkdir("/src")
+        .withMountedCache("/src/node_modules", dag.cacheVolume("node-modules"))
+        .withMountedCache("/usr/local/share/.cache/yarn", dag.cacheVolume("global-yarn-cache"))
+        .withExec(["yarn", "install"])
+        .withExec(["yarn", "setup"])
+        .withExec(["bash", "-c", "sed -i 's/localhost/0.0.0.0/' packages/legend-application-studio-deployment/studio.config.js"])
+    }
+
+    /**
+     * Legend Studio ready to run on localhost:9000
+     */ 
+	@func()
+    legendStudio(source: Directory): Container {
+        return this.studioBase(source)
+        .withExposedPort(9000)
+        .withExec(["yarn", "dev"])
+    }   
+
+    /**
+     * Minimal Legend Engine, Studio, and SDLC using gitlab.com
+     */ 
+    @func()
+    minimal(appId: Secret, appSecret: Secret): Service {
+        const engine = this.legendEngine(null, true)
+        const sdlc = this.legendSdlc(dag.git("https://github.com/finos/legend-sdlc").branch("master").tree(), "gitlab.com", "localhost", appId, appSecret)
+        const studio = this.legendStudio(dag.git("https://github.com/finos/legend-studio").branch("master").tree())
+        return dag.proxy()
+        .withService(engine.asService(), "engine", 6300, 6300)
+        .withService(sdlc.asService(), "sdlc", 6100, 6100)
+        .withService(studio.asService(), "studio", 9000, 9000)
+        .service()
+    }
+}   
